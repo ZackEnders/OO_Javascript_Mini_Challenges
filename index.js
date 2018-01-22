3342a9d403ee6aec361a5454135594fed2bf97d5
Zack Enders <enders.zack@gmail.com>
2018-01-22 17:41:25 -0500

    Added index files

---
+++ index.js
@@ -0,0 +1,10 @@
+// Create an object called Multiplier with two methods: multiply and getCurrentValue. 
+// multiply should initially return the number supplied * 1 and from then on whatever the 
+// current value is times the number supplied.getCurrentValue should return the last answer returned from multiply.
+
+
+function Multiplier(x){
+	multiply = x * 1;
+	var getCurrentValue = multiply
+}
+
